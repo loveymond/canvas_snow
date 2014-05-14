0ec6dc27cda382179b26305a62edbc2bd653769f
Mond Xunlei Master <loveymond@gmail.com>
2014-05-14 16:07:28 +0800

    First version.

---
+++ readme.md
@@ -0,0 +1,19 @@
+Readme
+======
+
+用Canvas做成的雪景。
+
+调用方法：
+
+```javascript
+
+    /**
+     * @param canvasId, String, canvas元素的ID
+     * @param speed, Float, optional, 雪花下落速度，0.01-100，默认值0.5
+     * @param density, Integer, optional, 雪花的密度，数值越小密度越大，最小为1，默认100
+     * @param snowColor, String, optional, 雪花的颜色，同canvas颜色，rgba用法与CSS不同，需要注意。默认#fff
+     */    
+    snow(canvasId, speed, density, snowColor)
+
+```
+
---
+++ snow.js
@@ -0,0 +1,101 @@
+var snow = function() {
+    var isCalled = 0;
+
+    return function(canvasId, speed, density, snowColor){
+        if (isCalled) {
+            return ;
+        } else {
+            isCalled = 1;
+        }
+
+        if (!snowColor) {
+            snowColor = "#fff";
+        }
+
+        density = parseInt(density);
+        if (density > 0 && density < 1000) {
+            //PASS
+        } else {
+            density = 100;
+        }
+
+        var canvasHeight = document.documentElement.clientHeight - 5,
+            canvasWidth = document.documentElement.clientWidth - 5,
+            snowNums = Math.ceil(canvasWidth * canvasHeight / density / density),
+            canvas = document.getElementById(canvasId),
+            context = canvas.getContext("2d"),
+            requestAnimFrame = (function(){
+                return  window.requestAnimationFrame
+                        || window.webkitRequestAnimationFrame
+                        || window.mozRequestAnimationFrame
+                        || window.oRequestAnimationFrame
+                        || window.msRequestAnimationFrame
+                        || function( callback ){
+                                window.setTimeout(callback, 10);
+                            };
+            })(),
+            i = 0;
+
+        // document.getElementById(canvasId).height = canvasHeight;
+        // document.getElementById(canvasId).width = canvasWidth;
+        canvas.height = canvasHeight;
+        canvas.width = canvasWidth;
+
+        function createFlower(context,n,dx,dy,size,length){
+            context.beginPath();
+            context.moveTo(dx,dy+size);
+            var dig = 2*Math.PI/n;
+            for(var i=1;i<n+1;i++){
+                var ctrlX = Math.sin((i-0.5)*dig)*length+dx;
+                var ctrlY = Math.cos((i-0.5)*dig)*length+dy;
+
+                var x = Math.sin(i*dig)*size+dx;
+                var y = Math.cos(i*dig)*size+dy;
+
+                context.quadraticCurveTo(ctrlX,ctrlY,x,y);
+            }
+            context.closePath();
+        }
+
+
+        snowPos = [];
+        for (; i < snowNums; i++) {
+            snowPos.push({
+                x:Math.round(Math.random() * canvasWidth),
+                y:Math.round(Math.random() * canvasHeight),
+                z:Math.round(Math.random() * 3 + 1)
+            });
+        }
+
+        var fall = function(){
+            //context.fillStyle = "#000";
+            // context.fillStyle = "rgba(0,0,0,0)";
+            // context.fillRect(0,0,420,280);
+            context.clearRect(0,0,canvasWidth,canvasHeight);
+            context.fillStyle = snowColor;
+            for(var i=0,len=snowPos.length;i<len;i++){
+                context.save();
+                context.translate(snowPos[i].x,snowPos[i].y);
+                // context.rotate((Math.random()*6-3)*Math.PI/10);
+
+                snowPos[i].x += Math.random()*0.03 - 0.01;
+                snowPos[i].y += Math.random()*speed;
+                if(snowPos[i].y>canvasHeight || snowPos[i].x < 0 || snowPos[i].x > canvasWidth){
+                    snowPos[i].y=4;
+                    snowPos[i].x = Math.round( Math.random() * canvasWidth );
+                    snowPos[i].z = Math.round( Math.random() * 3 + 1 );
+                }
+                createFlower(context,6,0,0,snowPos[i].z,8);
+                context.fill();
+                context.restore();
+            }
+        }
+
+        var cb = function(){
+            fall();
+            requestAnimFrame(cb);
+        };
+
+        cb();
+    };
+}();
