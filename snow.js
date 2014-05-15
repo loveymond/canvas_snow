8bb40462bfc10ef95a52a3fbc02f0f333d728f67
Mond Xunlei Master <loveymond@gmail.com>
2014-05-15 17:48:48 +0800

    Optimize the codes..

--- snow.js
+++ snow.js
@@ -8,17 +8,6 @@ var snow = function() {
             isCalled = 1;
         }
 
-        if (!snowColor) {
-            snowColor = "#fff";
-        }
-
-        density = parseInt(density);
-        if (density > 0 && density < 1000) {
-            //PASS
-        } else {
-            density = 100;
-        }
-
         var canvasHeight = document.documentElement.clientHeight - 5,
             canvasWidth = document.documentElement.clientWidth - 5,
             snowNums = Math.ceil(canvasWidth * canvasHeight / density / density),
@@ -34,27 +23,59 @@ var snow = function() {
                                 window.setTimeout(callback, 10);
                             };
             })(),
+            fall = function(){
+                context.clearRect(0,0,canvasWidth,canvasHeight);
+                context.fillStyle = snowColor;
+                for(var i=0,len=snowPos.length;i<len;i++){
+                    context.save();
+                    context.translate(snowPos[i].x,snowPos[i].y);
+
+                    snowPos[i].x += Math.random()*0.03 - 0.01;
+                    snowPos[i].y += Math.random()*speed;
+                    if(snowPos[i].y>canvasHeight || snowPos[i].x < 0 || snowPos[i].x > canvasWidth){
+                        snowPos[i].y=4;
+                        snowPos[i].x = Math.round( Math.random() * canvasWidth );
+                        snowPos[i].z = Math.round( Math.random() * 3 + 1 );
+                    }
+                    createFlower(context,6,0,0,snowPos[i].z,8);
+                    context.fill();
+                    context.restore();
+                }
+            },
+            cb = function(){
+                fall();
+                requestAnimFrame(cb);
+            },
+            createFlower = function(context,n,dx,dy,size,length){
+                context.beginPath();
+                context.moveTo(dx,dy+size);
+                var dig = 2*Math.PI/n;
+                for(var i=1;i<n+1;i++){
+                    var ctrlX = Math.sin((i-0.5)*dig)*length+dx;
+                    var ctrlY = Math.cos((i-0.5)*dig)*length+dy;
+
+                    var x = Math.sin(i*dig)*size+dx;
+                    var y = Math.cos(i*dig)*size+dy;
+
+                    context.quadraticCurveTo(ctrlX,ctrlY,x,y);
+                }
+                context.closePath();
+            },
             i = 0;
 
         canvas.height = canvasHeight;
         canvas.width = canvasWidth;
 
-        function createFlower(context,n,dx,dy,size,length){
-            context.beginPath();
-            context.moveTo(dx,dy+size);
-            var dig = 2*Math.PI/n;
-            for(var i=1;i<n+1;i++){
-                var ctrlX = Math.sin((i-0.5)*dig)*length+dx;
-                var ctrlY = Math.cos((i-0.5)*dig)*length+dy;
-
-                var x = Math.sin(i*dig)*size+dx;
-                var y = Math.cos(i*dig)*size+dy;
-
-                context.quadraticCurveTo(ctrlX,ctrlY,x,y);
-            }
-            context.closePath();
+        if (!snowColor) {
+            snowColor = "#fff";
         }
 
+        density = parseInt(density);
+        if (density > 0 && density < 1000) {
+            //PASS
+        } else {
+            density = 100;
+        }
 
         snowPos = [];
         for (; i < snowNums; i++) {
@@ -64,31 +85,6 @@ var snow = function() {
                 z:Math.round(Math.random() * 3 + 1)
             });
         }
-
-        var fall = function(){
-            context.clearRect(0,0,canvasWidth,canvasHeight);
-            context.fillStyle = snowColor;
-            for(var i=0,len=snowPos.length;i<len;i++){
-                context.save();
-                context.translate(snowPos[i].x,snowPos[i].y);
-
-                snowPos[i].x += Math.random()*0.03 - 0.01;
-                snowPos[i].y += Math.random()*speed;
-                if(snowPos[i].y>canvasHeight || snowPos[i].x < 0 || snowPos[i].x > canvasWidth){
-                    snowPos[i].y=4;
-                    snowPos[i].x = Math.round( Math.random() * canvasWidth );
-                    snowPos[i].z = Math.round( Math.random() * 3 + 1 );
-                }
-                createFlower(context,6,0,0,snowPos[i].z,8);
-                context.fill();
-                context.restore();
-            }
-        }
-
-        var cb = function(){
-            fall();
-            requestAnimFrame(cb);
-        };
 
         cb();
     };
